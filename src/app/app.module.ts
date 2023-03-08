4931526f4ddbd7c80ff88fce3129f649b21607ed
Yanggum <bak405@naver.com>
2023-03-08 14:53:40 +0900

    Init angular landing page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TechstackComponent } from './components/techstack/techstack.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ContactComponent,
+    TechstackComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/project.ts
@@ -0,0 +1,11 @@
+// project.ts
+
+export class Project {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  githubUrl: string;
+  liveUrl: string;
+  technologies: string[];
+}
